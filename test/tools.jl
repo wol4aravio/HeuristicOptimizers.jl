cc596337119049fa4d7147f881bac5a4dfd68c9f
wol4aravio <panovskiy.v@yandex.ru>
2022-05-12 10:01:15 +0300

    tests for generation

--- test/tools.jl
+++ test/tools.jl
@@ -8,4 +8,11 @@ using Test
     @test HeuristicOptimizers.push_to_domain([4, 4, 4], domain) == [1, 2, 3]
     @test HeuristicOptimizers.push_to_domain([-4, -4, -4], domain) == [-1, -2, -3]
     @test HeuristicOptimizers.push_to_domain([1.5, -2.5, 2.3], domain) == [1, -2, 2.3]
+
+    dummy_test_var = true
+    for _ in range(1, 100)
+        x = HeuristicOptimizers.generate_vector_in_area(domain)
+        dummy_test_var &=  x == HeuristicOptimizers.push_to_domain(x, domain)
+    end
+    @test dummy_test_var
 end
