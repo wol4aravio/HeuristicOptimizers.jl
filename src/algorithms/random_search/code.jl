9945ab63f43f3920c25528ad2335bcc0bc8da724
wol4aravio <panovskiy.v@yandex.ru>
2022-05-19 07:45:26 +0300

    add empty line to RS

--- src/algorithms/random_search/code.jl
+++ src/algorithms/random_search/code.jl
@@ -2,6 +2,7 @@ include("../common/abstracts.jl")
 include("../common/problem.jl")
 include("../common/base_state.jl")
 
+
 mutable struct State
     x::Union{Vector{<:Real}, Nothing}
     f::Union{Real, Nothing}
